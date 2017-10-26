d3b7673f2fbf8e86371b964bcd21171dc3bacefc
Kane <xiaoyunhua@shhxzq.com>
2017-10-26 17:50:45 +0800

    第三方工具库选型

--- src/index.js
+++ src/index.js
@@ -3,4 +3,14 @@ export * as Storage from './storage';
 export * as Log from './log';
 export * as cookies from './cookies';
 
-// qs、ua-parser-js
+/**
+ * 其他第三方工具库
+ *
+ * `qs` 查询字符串 parse/stringify，支持多层级及数组
+ * `ua-parser-js` 浏览器 userAgent 分析，支持国产手机型号
+ * `format-num` 格式化数字，小数位、千分符
+ * `number-unit` 数字单位自定义
+ * `parse-num` 解析数字
+ * `moment` 日期时间处理 moment.utc('20171022152344', 'YYYYMMDDHHmmss').valueOf()
+ * `js-cookie` 浏览器 cookie set get remove 操作，支持 cookie 编码解码
+ */
