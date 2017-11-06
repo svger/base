9ac1a1d528b53356ab5185bda88d213df8b3d444
Kane <xiaoyunhua@shhxzq.com>
2017-11-06 14:35:29 +0800

    change name

--- build/redux-store.js
+++ build/redux-store.js
@@ -13,14 +13,23 @@ var _reduxThunk2 = _interopRequireDefault(_reduxThunk);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } } /**
+                                                                                                                                                                                                     * utils/redux-store
+                                                                                                                                                                                                     *
+                                                                                                                                                                                                     * actions、reducers 可以按业务模块写在一个文件中，并通过 addReducer 动态添加到 store
+                                                                                                                                                                                                     */
+
 
 var rootReducer = function rootReducer() {};
-var reducerCache = {}; //保存更新后的reducers列表
-var storeCache = {}; //保存更新后的store状态
+var reducerCache = {};
+var storeCache = {};
 
-// applyMiddleware来自redux可以包装 store 的 dispatch
-// thunk作用是使被 dispatch 的 function 会接收 dispatch 作为参数，并且可以异步调用它;
+/**
+ * 首次配置 Store, 设置初始 state 及中间件
+ *
+ * @param {Object} preloadedState 初始 state
+ * @param {Array} middlewares 中间件列表
+ */
 var configureStore = exports.configureStore = function configureStore(preloadedState) {
   var middlewares = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [_reduxThunk2.default];
 
@@ -39,6 +48,10 @@ var configureStore = exports.configureStore = function configureStore(preloadedS
   return storeCache;
 };
 
+/**
+ * 动态添加 reducer
+ * @param {Object} reducers redux reducers
+ */
 var addReducer = exports.addReducer = function addReducer(reducers) {
   var reducerKeys = Object.keys(reducers);
   reducerKeys.forEach(function (key) {
